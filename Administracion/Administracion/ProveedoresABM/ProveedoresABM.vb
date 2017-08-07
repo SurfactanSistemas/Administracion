83a69439954d2c1447cc007e09f0d645b70867fb
Maximiliano Gastón Ferreyra <fergthh@gmail.com>
2017-08-07 15:43:18 -0300

    Se permiten solo numeros en campos: codigo, codigo postal, telefono y cuenta contable.

--- Administracion/Administracion/ProveedoresABM/ProveedoresABM.vb
+++ Administracion/Administracion/ProveedoresABM/ProveedoresABM.vb
@@ -1424,4 +1424,10 @@ Public Class ProveedoresABM
         End If
 
     End Sub
+
+    Private Sub SoloNumero(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtCodigo.KeyPress, txtTelefono.KeyPress, txtCodigoPostal.KeyPress, txtCuenta.KeyPress
+        If Not Char.IsNumber(e.KeyChar) And Not Char.IsControl(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
 End Class
