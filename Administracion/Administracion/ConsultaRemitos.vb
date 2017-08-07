0cade5178cb8ac35756db9f163783e4548cfe111
Maximiliano Gastón Ferreyra <fergthh@gmail.com>
2017-08-07 16:00:46 -0300

    En consulta de remitos, solo se permiten numeros en campo de codigo de proveedor.

--- Administracion/Administracion/ConsultaRemitos.vb
+++ Administracion/Administracion/ConsultaRemitos.vb
@@ -264,4 +264,14 @@ Public Class ConsultaRemitos
         txtFiltrar.Text = ""
         txtFiltrar.Focus()
     End Sub
+
+
+    Private Sub SoloNumero(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtCodigoProveedor.KeyPress
+        If Not Char.IsNumber(e.KeyChar) And Not Char.IsControl(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
+
+
 End Class
--- Administracion/Administracion/RubrosProveedorABM.vb
+++ Administracion/Administracion/RubrosProveedorABM.vb
@@ -301,4 +301,10 @@ Public Class RubrosProveedorABM
         End If
 
     End Sub
+
+    Private Sub SoloNumero(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtCodigo.KeyPress
+        If Not Char.IsNumber(e.KeyChar) And Not Char.IsControl(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
 End Class
