b2156653134cc6b1700639c644cfdbc066b1704a
Gaston Ferreyra <fergthh@gmail.com>
2017-11-07 12:55:44 -0300

    Comando: Se comienza a generar reportes.

---
+++ Comando/Comando/ComparacionEntreFamiliasPeriodos.vb
@@ -0,0 +1,148 @@
+﻿Imports System.Data.SqlClient
+
+Public Class ComparacionEntreFamiliasPeriodos
+
+    Private DT As New DataTable("Detalles")
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Select Case cmbPeriodoComparacion.SelectedIndex
+            Case 1 ' Comparacion por mes.
+                _ComparacionMensual()
+            Case 2 ' Comparacion por bimestre
+
+            Case 3 ' Comparacion por trimestre
+
+            Case 4 ' Comparacion por semestre
+
+            Case Else
+                MsgBox("Debe elegir un periodo válido de comparación.", MsgBoxStyle.Exclamation)
+                Exit Sub
+        End Select
+
+    End Sub
+
+    Private Sub _ComparacionMensual()
+
+        Select Case cmbTipoComparacion.SelectedIndex
+            Case 1 ' Valor Unico
+                _ComparacionValorUnicoMensual()
+            Case 2 ' Comparacion entre Valores
+
+            Case Else
+                MsgBox("Debe elegir un tipo de comparacion válido.", MsgBoxStyle.Exclamation)
+                Exit Sub
+        End Select
+
+    End Sub
+
+    Private Function _PrepararVentasMensuales() As DataTable
+
+        Dim tabla As New DataTable("Detalles")
+        Dim _row As DataRow
+
+        With tabla
+            .Columns.Add("Corte")
+            .Columns.Add("Tipo")
+            .Columns.Add("Descripcion")
+            .Columns.Add("Venta", System.Type.GetType("System.Double"))
+        End With
+
+        For corte = 1 To 12
+
+            For Each row As DataRow In DT.Rows
+
+                If Val(row.Item("Venta" & corte)) <> 0 Then
+
+                    _row = tabla.NewRow
+
+                    With _row
+                        .Item("Corte") = corte
+                        .Item("Tipo") = row.Item("Tipo")
+                        .Item("Descripcion") = row.Item("Descripcion")
+                        .Item("Venta") = row.Item("Venta" & corte)
+                    End With
+
+                    tabla.Rows.Add(_row)
+
+                End If
+
+            Next
+
+        Next
+        
+        Return tabla
+
+    End Function
+
+    Private Sub _ComparacionValorUnicoMensual()
+        Select Case cmbValoresUnicos.SelectedIndex
+            Case 1 ' Ventas
+
+                Dim tabla As DataTable = _PrepararVentasMensuales()
+
+                With VistaPrevia
+                    .Reporte = New VentasMensualesBarras
+                    .Reporte.SetDataSource(tabla)
+                    .Mostrar()
+                End With
+
+            Case 2 ' Kilos
+
+            Case 3 ' Factor
+
+            Case 4 ' Precio
+
+            Case 5 ' Stock
+
+            Case 6 ' Rotacion
+
+            Case 7 ' Porcentaje
+
+            Case 8 ' Pedidos
+
+            Case 9 ' Atrasos
+
+            Case 10 ' % Atrasos
+
+            Case Else
+                MsgBox("Debe elegir un valor valido de comparacion.", MsgBoxStyle.Exclamation)
+                Exit Sub
+        End Select
+
+    End Sub
+
+    Private Sub ComparacionEntreFamiliasPeriodos_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        _CargarDatos()
+    End Sub
+
+    Private Sub _CargarDatos()
+        Dim cn As SqlConnection = New SqlConnection()
+        Dim cm As SqlCommand = New SqlCommand("SELECT * FROM Comando")
+        Dim dr As SqlDataReader
+
+        Try
+            DT.Rows.Clear()
+            cn.ConnectionString = Helper._ConectarA
+            cn.Open()
+            cm.Connection = cn
+
+            dr = cm.ExecuteReader()
+
+            If dr.HasRows Then
+                DT.Load(dr)
+            End If
+
+        Catch ex As Exception
+            Throw New Exception("Hubo un problema al querer consultar los datos de Comando desde la Base de Datos." & vbCrLf & vbCrLf & "Motivo: " & ex.Message)
+        Finally
+
+            dr = Nothing
+            cn.Close()
+            cn = Nothing
+            cm = Nothing
+
+        End Try
+
+    End Sub
+End Class
---
+++ Comando/Comando/MenuPrincipal.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MenuPrincipal
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MaestrosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PruebaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ProcesosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CerrarSistemaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ComparaciónDeFamiliasEntreMismosPeriodosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MaestrosToolStripMenuItem, Me.ProcesosToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(787, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MaestrosToolStripMenuItem
+        '
+        Me.MaestrosToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PruebaToolStripMenuItem, Me.ComparaciónDeFamiliasEntreMismosPeriodosToolStripMenuItem})
+        Me.MaestrosToolStripMenuItem.Name = "MaestrosToolStripMenuItem"
+        Me.MaestrosToolStripMenuItem.Size = New System.Drawing.Size(67, 20)
+        Me.MaestrosToolStripMenuItem.Text = "Maestros"
+        '
+        'PruebaToolStripMenuItem
+        '
+        Me.PruebaToolStripMenuItem.Name = "PruebaToolStripMenuItem"
+        Me.PruebaToolStripMenuItem.Size = New System.Drawing.Size(332, 22)
+        Me.PruebaToolStripMenuItem.Text = "Prueba"
+        '
+        'ProcesosToolStripMenuItem
+        '
+        Me.ProcesosToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CerrarSistemaToolStripMenuItem})
+        Me.ProcesosToolStripMenuItem.Name = "ProcesosToolStripMenuItem"
+        Me.ProcesosToolStripMenuItem.Size = New System.Drawing.Size(66, 20)
+        Me.ProcesosToolStripMenuItem.Text = "Procesos"
+        '
+        'CerrarSistemaToolStripMenuItem
+        '
+        Me.CerrarSistemaToolStripMenuItem.Name = "CerrarSistemaToolStripMenuItem"
+        Me.CerrarSistemaToolStripMenuItem.Size = New System.Drawing.Size(150, 22)
+        Me.CerrarSistemaToolStripMenuItem.Text = "Cerrar Sistema"
+        '
+        'ComparaciónDeFamiliasEntreMismosPeriodosToolStripMenuItem
+        '
+        Me.ComparaciónDeFamiliasEntreMismosPeriodosToolStripMenuItem.Name = "ComparaciónDeFamiliasEntreMismosPeriodosToolStripMenuItem"
+        Me.ComparaciónDeFamiliasEntreMismosPeriodosToolStripMenuItem.Size = New System.Drawing.Size(332, 22)
+        Me.ComparaciónDeFamiliasEntreMismosPeriodosToolStripMenuItem.Text = "Comparación de Familias entre mismos periodos"
+        '
+        'MenuPrincipal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(787, 428)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "MenuPrincipal"
+        Me.Text = "Menú Principal"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents MaestrosToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ProcesosToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CerrarSistemaToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PruebaToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ComparaciónDeFamiliasEntreMismosPeriodosToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
