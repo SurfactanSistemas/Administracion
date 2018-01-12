d51b8b9b8bf515cd92031400115abcff0061e52a
Gaston Ferreyra <fergthh@gmail.com>
2018-01-12 14:25:38 -0300

    Identificaciones: Se agrega restriccion de solo numero en campos de DNI y Proveedor.

--- Identificaciones/Identificaciones/Form1.vb
+++ Identificaciones/Identificaciones/Form1.vb
@@ -765,4 +765,11 @@ Public Class Form1
         End With
 
     End Sub
+
+    Private Sub SoloNumero(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtNroDocumento.KeyPress, txtProveedor.KeyPress
+        If Not Char.IsNumber(e.KeyChar) And Not Char.IsControl(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
 End Class
