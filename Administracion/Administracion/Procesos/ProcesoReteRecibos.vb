9ff947461cf9fd109af2aaa37dcc532db95f6c4c
Maximiliano Gastón Ferreyra <fergthh@gmail.com>
2018-02-06 14:33:38 -0300

    Administracion: Se reescribe Proceso de Retenciones de IB en Recibos.

--- Administracion/Administracion/Procesos/ProcesoReteRecibos.vb
+++ Administracion/Administracion/Procesos/ProcesoReteRecibos.vb
@@ -1,6 +1,7 @@
 ﻿Imports System.Data.SqlClient
 Imports ClasesCompartidas
 Imports System.IO
+Imports System.Runtime.Remoting
 
 Public Class ProcesoReteRecibos
 
@@ -116,7 +117,7 @@ Public Class ProcesoReteRecibos
                     WCampo3 = WFecha
                     WCampo4 = "078"
                     WCampo6 = WImporte.Replace(",", ".")
-                    WCampo5 = WComproganan
+                    WCampo5 = WComproGanan
 
                     escritor.Write(WCampo1 + WCampo2 + WCampo3 + WCampo4 + WCampo6 + WCampo5 + vbCrLf)
 
@@ -132,38 +133,153 @@ Public Class ProcesoReteRecibos
                 Dim ZRetIb(100, 3) As Double
                 Dim WIndice = 0
 
+                ZRetIb(1, 2) = 901
+                ZRetIb(2, 2) = 902
+                ZRetIb(3, 2) = 904
+                ZRetIb(4, 2) = 919
+                ZRetIb(5, 2) = 921
+                ZRetIb(6, 2) = 923
+                ZRetIb(7, 2) = 924
+
                 tabla = _TraerRetencionesIBRecibos(ordDesde, ordHasta) 'SQLConnector.retrieveDataTable("procesoReteIbrecibos", ordDesde, ordHasta)
 
                 For Each row As DataRow In tabla.Rows
 
-                    Vector(WIndice, 0) = ""
+                    With row
 
-                    '    Dim CamposReteIb As New ProcesoReteIbRecibos(row.Item(0).ToString, row.Item(1), row.Item(2).ToString, row.Item(3).ToString, row.Item(4).ToString, row.Item(5).ToString, row.Item(6).ToString)
+                        Vector(WIndice, 0) = .Item("Cuit")
+                        Vector(WIndice, 1) = .Item("Cliente")
+                        Vector(WIndice, 2) = .Item("Fecha")
+                        Vector(WIndice, 3) = IIf(IsDBNull(.Item("ComproIB")), "", .Item("ComproIB"))
+                        Vector(WIndice, 4) = Str$(.Item("RetOtra"))
+                        Vector(WIndice, 5) = Str$(.Item("Recibo"))
 
-                    '    WCuit = leederecha(CamposReteIb.cuit, 13)
-                    '    WFecha = CamposReteIb.fecha
-                    '    WFecha = "01/01/2016"
-                    '    WComproIb = ceros(CamposReteIb.comproib, 16)
-                    '    WImporte = ceros(formatonumerico(redondeo(CamposReteIb.retotra), "########0.#0", "."), 11)
-                    '    WRecibo = "121212121"
+                        ZRetIb(1, 1) = IIf(IsDBNull(.Item("RetIb1")), 0, .Item("RetIb1"))
+                        ZRetIb(2, 1) = IIf(IsDBNull(.Item("RetIb2")), 0, .Item("RetIb2"))
+                        ZRetIb(3, 1) = IIf(IsDBNull(.Item("RetIb3")), 0, .Item("RetIb3"))
+                        ZRetIb(4, 1) = IIf(IsDBNull(.Item("RetIb4")), 0, .Item("RetIb4"))
+                        ZRetIb(5, 1) = IIf(IsDBNull(.Item("RetIb5")), 0, .Item("RetIb5"))
+                        ZRetIb(6, 1) = IIf(IsDBNull(.Item("RetIb6")), 0, .Item("RetIb6"))
+                        ZRetIb(7, 1) = IIf(IsDBNull(.Item("RetIb7")), 0, .Item("RetIb7"))
 
-                    '    WCampo1 = "902"
-                    '    WCampo2 = WCuit
-                    '    WCampo3 = WFecha
-                    '    WCampo4 = "0001"
-                    '    WCampo5 = WComproIb
-                    '    WCampo6 = "R"
-                    '    WCampo7 = "A"
-                    '    WCampo8 = WRecibo
-                    '    WCampo9 = WImporte
+                        Vector(WIndice, 6) = Str$(ZRetIb(1, 1))
+                        Vector(WIndice, 7) = Str$(ZRetIb(2, 1))
+                        Vector(WIndice, 8) = Str$(ZRetIb(3, 1))
+                        Vector(WIndice, 9) = Str$(ZRetIb(4, 1))
+                        Vector(WIndice, 10) = Str$(ZRetIb(5, 1))
+                        Vector(WIndice, 11) = Str$(ZRetIb(6, 1))
+                        Vector(WIndice, 12) = Str$(ZRetIb(7, 1))
 
-                    '    escritor.Write(WCampo1 + WCampo2 + WCampo3 + WCampo4 + WCampo5 + WCampo6 + WCampo7 + WCampo8 + WCampo9 + vbCrLf)
+                        ZRetIb(1, 3) = IIf(IsDBNull(.Item("NroRetIb1")), 0, .Item("NroRetIb1"))
+                        ZRetIb(2, 3) = IIf(IsDBNull(.Item("NroRetIb2")), 0, .Item("NroRetIb2"))
+                        ZRetIb(3, 3) = IIf(IsDBNull(.Item("NroRetIb3")), 0, .Item("NroRetIb3"))
+                        ZRetIb(4, 3) = IIf(IsDBNull(.Item("NroRetIb4")), 0, .Item("NroRetIb4"))
+                        ZRetIb(5, 3) = IIf(IsDBNull(.Item("NroRetIb5")), 0, .Item("NroRetIb5"))
+                        ZRetIb(6, 3) = IIf(IsDBNull(.Item("NroRetIb6")), 0, .Item("NroRetIb6"))
+                        ZRetIb(7, 3) = IIf(IsDBNull(.Item("NroRetIb7")), 0, .Item("NroRetIb7"))
+
+                        Vector(WIndice, 13) = Str$(ZRetIb(1, 3))
+                        Vector(WIndice, 14) = Str$(ZRetIb(2, 3))
+                        Vector(WIndice, 15) = Str$(ZRetIb(3, 3))
+                        Vector(WIndice, 16) = Str$(ZRetIb(4, 3))
+                        Vector(WIndice, 17) = Str$(ZRetIb(5, 3))
+                        Vector(WIndice, 18) = Str$(ZRetIb(6, 3))
+                        Vector(WIndice, 19) = Str$(ZRetIb(7, 3))
+
+                    End With
 
                     WIndice += 1
 
                 Next
 
-                'escritor.Close()
+                Dim WCliente = "", WRetOtra = ""
+
+                For i = 0 To WIndice
+
+                    WCuit = Vector(i, 0)
+                    WCuit = _left(WCuit, 13)
+                    WCliente = Vector(i, 1)
+                    WFecha = Vector(i, 2)
+                    WComproIb = Vector(i, 3)
+                    WImporte = Vector(i, 4)
+                    WRecibo = Vector(i, 5)
+                    WRetOtra = Vector(i, 4)
+
+                    WImporte = Proceso.formatonumerico(WImporte)
+                    WImporte = WImporte.Replace(".", ",")
+
+                    ZRetIb(1, 1) = Val(Vector(i, 6))
+                    ZRetIb(2, 1) = Val(Vector(i, 7))
+                    ZRetIb(3, 1) = Val(Vector(i, 8))
+                    ZRetIb(4, 1) = Val(Vector(i, 9))
+                    ZRetIb(5, 1) = Val(Vector(i, 10))
+                    ZRetIb(6, 1) = Val(Vector(i, 11))
+                    ZRetIb(7, 1) = Val(Vector(i, 12))
+
+                    ZRetIb(1, 3) = Val(Vector(i, 13))
+                    ZRetIb(2, 3) = Val(Vector(i, 14))
+                    ZRetIb(3, 3) = Val(Vector(i, 15))
+                    ZRetIb(4, 3) = Val(Vector(i, 16))
+                    ZRetIb(5, 3) = Val(Vector(i, 17))
+                    ZRetIb(6, 3) = Val(Vector(i, 18))
+                    ZRetIb(7, 3) = Val(Vector(i, 19))
+
+                    For x = 1 To 7
+
+                        If ZRetIb(x, 1) <> 0 Then
+                            WRetOtra = "0"
+                        End If
+
+                    Next
+
+                    If Val(WRetOtra) <> 0 Then
+
+                        WCampo1 = "902"
+                        WCampo2 = WCuit
+                        WCampo3 = Trim(WFecha)
+                        WCampo4 = "0001"
+                        WCampo5 = ceros(WComproIb, 16)
+                        WCampo6 = "R"
+                        WCampo7 = "A"
+                        WCampo8 = ceros(WRecibo, 20)
+                        WCampo9 = ceros(WImporte, 11)
+
+                        escritor.Write(WCampo1 + WCampo2 + WCampo3 + WCampo4 + WCampo5 + WCampo6 + WCampo7 + WCampo8 + WCampo9 + vbCrLf)
+
+                    Else
+
+                        For j = 1 To 7
+
+                            If ZRetIb(j, 1) <> 0 Then
+
+                                WImporte = Str$(ZRetIb(j, 1))
+                                WComproIb = Str$(ZRetIb(j, 3))
+
+                                WImporte = Proceso.formatonumerico(WImporte)
+                                WImporte = WImporte.Replace(".", ",")
+
+                                WCampo1 = Str$(ZRetIb(j, 2))
+                                WCampo1 = ceros(WCampo1, 3)
+                                WCampo2 = WCuit
+                                WCampo3 = Trim(WFecha)
+                                WCampo4 = "0001"
+                                WCampo5 = ceros(WComproIb, 16)
+                                WCampo6 = "R"
+                                WCampo7 = "A"
+                                WCampo8 = ceros(WRecibo, 20)
+                                WCampo9 = ceros(WImporte, 11)
+
+                                escritor.Write(WCampo1 + WCampo2 + WCampo3 + WCampo4 + WCampo5 + WCampo6 + WCampo7 + WCampo8 + WCampo9 + vbCrLf)
+
+                            End If
+
+                        Next
+
+                    End If
+
+                Next
+
+                escritor.Close()
 
                 MsgBox("Proceso Finalizado de Retenciones de Ingresos Brutos", MsgBoxStyle.Information)
 
@@ -177,7 +293,9 @@ Public Class ProcesoReteRecibos
         Dim tabla As New DataTable
 
         Dim cn As SqlConnection = New SqlConnection()
-        Dim cm As SqlCommand = New SqlCommand("SELECT * FROM Recibos WHERE FechaOrd BETWEEN " & WDesde & " AND " & WHasta & " RetOtra <> 0 AND Renglon = 1")
+        Dim cm As SqlCommand = New SqlCommand("SELECT r.Cliente, r.Fecha, r.ComproIb, r.RetOtra, r.Recibo, cli.Cuit, " & _
+                                              " RetIb1,RetIb2,RetIb3,RetIb4,RetIb5,RetIb6,RetIb7, NroRetIb1, NroRetIb2, " & _
+                                              " NroRetIb3, NroRetIb4, NroRetIb5, NroRetIb6, NroRetIb7 FROM Recibos r JOIN Cliente cli ON cli.Cliente = r.Cliente WHERE FechaOrd BETWEEN " & WDesde & " AND " & WHasta & " AND RetOtra <> 0 AND Renglon = 1")
         Dim dr As SqlDataReader
 
         Try
