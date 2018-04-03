2f035b302448e3f121808dd9c1b802bb40f166c6
Maximiliano Gastón Ferreyra <fergthh@gmail.com>
2018-04-03 12:58:10 -0300

    Capacitacion: Se refactoriza Módulo de Sectores.

--- Capacitacion/Modulo Capacitacion/Maestros/Sectores/Sectores_Inicio.cs
+++ Capacitacion/Modulo Capacitacion/Maestros/Sectores/Sectores_Inicio.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Data;
-using System.Drawing;
 using System.Windows.Forms;
+using CrystalDecisions.CrystalReports.Engine;
+using Modulo_Capacitacion.Listados;
+using Modulo_Capacitacion.Listados.Sectores;
 using Negocio;
 
 namespace Modulo_Capacitacion.Maestros.Sectores
@@ -9,69 +11,12 @@ namespace Modulo_Capacitacion.Maestros.Sectores
     public partial class Sectores_Inicio : Form
     {
         Sector sector = new Sector();
-        private DataTable WOriginal;
 
         public Sectores_Inicio()
         {
             InitializeComponent();
             DGV_Sectores.DataSource = sector.ListarTodosCodigoDescripcion();
-            WOriginal = (DataTable) DGV_Sectores.DataSource;
         }
-
-        //private void BT_Filtrar_Click(object sender, EventArgs e)
-        //{
-        //    if (BT_Filtrar.Text == "Filtrar")
-        //    {
-        //        try
-        //        {
-        //            DataTable dataTable = DGV_Sectores.DataSource as DataTable;
-        //            if (dataTable != null)
-        //                dataTable.DefaultView.RowFilter = string.Format("CONVERT(" + LBFiltro.Text + ", System.String) like '%{0}%'", TBFiltro.Text);
-        //            BT_Filtrar.Text = "Limp. Filtro";
-        //        }
-        //        catch (Exception err)
-        //        {
-        //            MessageBox.Show(err.Message,"Error");
-        //        }                
-        //    }
-        //    else
-        //    {
-        //        DataTable dataTable = DGV_Sectores.DataSource as DataTable;
-        //        if (dataTable != null)
-        //            dataTable.DefaultView.RowFilter = string.Empty;
-        //        P_Filtrado.Visible = false;
-        //        TBFiltro.Text = "";
-        //        BT_Filtrar.Visible = true;
-        //    }
-        //}
-
-        //private void BT_MenuFiltros_Click(object sender, EventArgs e)
-        //{
-        //    Button btnSender = (Button)sender;
-        //    Point ptLowerLeft = new Point(0, btnSender.Height);
-        //    ptLowerLeft = btnSender.PointToScreen(ptLowerLeft);            
-        //    CMS_Sectores.Show(ptLowerLeft);
-
-
-        //    BT_Filtrar.Text = "Limp. Filtro";
-        //    TBFiltro.Text = "";
-        //}
-
-        //private void codigoToolStripMenuItem_Click(object sender, EventArgs e)
-        //{
-        //    P_Filtrado.Visible = true;
-        //    TBFiltro.Text = "";
-        //    LBFiltro.Text = "Codigo";
-        //    TBFiltro.Focus();
-        //}
-
-        //private void descripcionToolStripMenuItem_Click(object sender, EventArgs e)
-        //{
-        //    P_Filtrado.Visible = true;
-        //    TBFiltro.Text = "";
-        //    LBFiltro.Text = "Descripcion";
-        //    TBFiltro.Focus();
-        //}
 
         private void BTAgregarSector_Click(object sender, EventArgs e)
         {
@@ -112,16 +57,6 @@ namespace Modulo_Capacitacion.Maestros.Sectores
             DGV_Sectores.DataSource = sector.ListarTodosCodigoDescripcion();
         }
 
-        //private void TBFiltro_KeyDown(object sender, KeyEventArgs e)
-        //{
-        //    if (e.KeyCode == Keys.Enter)
-        //    {
-        //        BT_Filtrar.PerformClick();
-        //        e.SuppressKeyPress = true;
-        //        e.Handled = true;
-        //    }
-        //}
-
         private void BTModifSector_Click(object sender, EventArgs e)
         {
             try
@@ -150,11 +85,6 @@ namespace Modulo_Capacitacion.Maestros.Sectores
         {
             Close();
         }
-
-        //private void TBFiltro_TextChanged(object sender, EventArgs e)
-        //{
-        //    BT_Filtrar.Text = "Filtrar";
-        //}
 
         private void DGV_Sectores_RowHeaderMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
         {
@@ -188,8 +118,8 @@ namespace Modulo_Capacitacion.Maestros.Sectores
 
         private void btnImprimir_Click(object sender, EventArgs e)
         {
-            CrystalDecisions.CrystalReports.Engine.ReportDocument reporte = new Listados.Sectores.ListadoSectores();
-            Listados.VistaPrevia rp = new Listados.VistaPrevia();
+            ReportDocument reporte = new ListadoSectores();
+            VistaPrevia rp = new VistaPrevia();
 
             rp.CargarReporte(reporte);
 
