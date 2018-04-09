0735d90e1599051ca178053daff5ce094ba5c732
Maximiliano Gastón Ferreyra <fergthh@gmail.com>
2018-04-09 12:39:06 -0300

    Capacitacion: Se refactoriza Pantalla Inicio.

--- Capacitacion/Modulo Capacitacion/Inicio.cs
+++ Capacitacion/Modulo Capacitacion/Inicio.cs
@@ -1,16 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
+using Modulo_Capacitacion.Listados.Perfil;
+using Modulo_Capacitacion.Maestros.Cursos;
+using Modulo_Capacitacion.Maestros.Legajos;
+using Modulo_Capacitacion.Maestros.Perfiles;
 using Modulo_Capacitacion.Maestros.Sectores;
 using Modulo_Capacitacion.Maestros.Temas;
-using Modulo_Capacitacion.Maestros.Cursos;
-using Modulo_Capacitacion.Maestros.Perfiles;
-using Modulo_Capacitacion.Maestros.Legajos;
 using Modulo_Capacitacion.Maestros.VersionLegajo;
 using Modulo_Capacitacion.Maestros.VersionPerfiles;
 using Modulo_Capacitacion.Novedades;
@@ -48,66 +43,60 @@ namespace Modulo_Capacitacion
 
         private void ingresoDeTemasToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Temas_Inicio temasForm = new Temas_Inicio();
-            temasForm.StartPosition = FormStartPosition.CenterScreen;
+            Temas_Inicio temasForm = new Temas_Inicio {StartPosition = FormStartPosition.CenterScreen};
             temasForm.ShowDialog();
         }
 
         private void ingresoDeCursosToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Cursos_Inicio cursosForm = new Cursos_Inicio();
-            cursosForm.StartPosition = FormStartPosition.CenterScreen;
+            Cursos_Inicio cursosForm = new Cursos_Inicio {StartPosition = FormStartPosition.CenterScreen};
             cursosForm.ShowDialog();
         }
 
         private void ingresoDePerfilesToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Perfiles_Inicio perfilesform = new Perfiles_Inicio();
-            perfilesform.StartPosition = FormStartPosition.CenterScreen;
+            Perfiles_Inicio perfilesform = new Perfiles_Inicio {StartPosition = FormStartPosition.CenterScreen};
             perfilesform.ShowDialog();
         }
 
         private void ingresoDeLegajosToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Legajos_Inicio form = new Legajos_Inicio();
-            form.StartPosition = FormStartPosition.CenterScreen;
+            Legajos_Inicio form = new Legajos_Inicio {StartPosition = FormStartPosition.CenterScreen};
             form.ShowDialog();
         }
 
         private void consultaDeVersionDeLegajosToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            IngVersLegajo form = new IngVersLegajo();
-            form.StartPosition = FormStartPosition.CenterScreen;
+            IngVersLegajo form = new IngVersLegajo {StartPosition = FormStartPosition.CenterScreen};
             form.ShowDialog();
         }
 
         private void consultaDeVersionDePerfilesToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            IngVersPerfil form = new IngVersPerfil();
-            form.StartPosition = FormStartPosition.CenterScreen;
+            IngVersPerfil form = new IngVersPerfil {StartPosition = FormStartPosition.CenterScreen};
             form.ShowDialog();
         }
 
         private void ingresoDeCornogramaDeCapacitaciónToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            IngreDeCursosRealizados form = new IngreDeCursosRealizados();
-            form.StartPosition = FormStartPosition.CenterScreen;
+            IngreDeCursosRealizados form = new IngreDeCursosRealizados {StartPosition = FormStartPosition.CenterScreen};
             form.Show();
         }
 
         private void ingresoDeCursosRealizadosToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            IngresoDeCursosRealizados form = new IngresoDeCursosRealizados();
-            form.StartPosition = FormStartPosition.CenterScreen;
+            IngresoDeCursosRealizados form = new IngresoDeCursosRealizados
+            {
+                StartPosition = FormStartPosition.CenterScreen
+            };
             form.ShowDialog();
             
         }
 
         private void listadoDeLegajosPorPerfilToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Listados.Perfil.Inicio Ini = new Listados.Perfil.Inicio();
-            
-            Ini.StartPosition = FormStartPosition.CenterScreen;
+            Inicio Ini = new Inicio {StartPosition = FormStartPosition.CenterScreen};
+
             Ini.ShowDialog();
         }
 
