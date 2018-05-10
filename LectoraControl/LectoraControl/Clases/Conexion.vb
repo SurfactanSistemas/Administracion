352a37574c60b6b5f9e95b541cedb863c8ee031c
Maximiliano Gastón Ferreyra <fergthh@gmail.com>
2018-05-10 08:49:49 -0300

    LectoraControl: Se termina una demostracion.

---
+++ LectoraControl/LectoraControl/Clases/Conexion.vb
@@ -0,0 +1,260 @@
+﻿Imports System.Data.SqlClient
+Imports System.Configuration
+
+Namespace Clases
+
+    Public Class Conexion
+
+        Public Shared EsPellital As Boolean = False
+        Private Shared _EmpresaTrabajo As String = ""
+        Private Shared _Operador As String = ""
+        Public Shared WAtributosOperador(10, 100) As String
+
+        Public Shared Property Operador As String
+
+            Get
+
+                Return IIf(_Operador = "", 0, _Operador)
+
+            End Get
+
+            Private Set(ByVal value As String)
+                _Operador = value
+            End Set
+
+        End Property
+
+        Public Shared Property EmpresaDeTrabajo As String
+            Get
+                Return _EmpresaTrabajo
+
+            End Get
+            Set(ByVal value As String)
+
+                _EmpresaTrabajo = DeterminarBasePara(value)
+
+                If UCase(_EmpresaTrabajo).StartsWith("SURFACTAN") Then
+
+                    EsPellital = False
+
+                ElseIf UCase(_EmpresaTrabajo).StartsWith("PELLITAL") Or UCase(_EmpresaTrabajo).StartsWith("PELITALL") Then
+
+                    EsPellital = True
+
+                End If
+
+            End Set
+        End Property
+
+
+        Public Shared ReadOnly Property Empresas As List(Of String)
+            Get
+                ' DETERMINO LAS EMPRESAS CON LAS QUE TRABAJAR.
+
+                Dim WEmpresa = "SURFACTAN"
+
+                If EsPellital Then
+                    WEmpresa = "PELLITAL"
+                End If
+
+                Select Case UCase(WEmpresa)
+
+                    Case "SURFACTAN", "LOCAL"
+
+                        Dim lista() = {"SurfactanSA", "surfactan_II", "Surfactan_III", "Surfactan_IV", "Surfactan_V", "Surfactan_VI", "Surfactan_VII"}
+
+                        Return New List(Of String)(lista)
+
+                    Case "PELLITAL"
+                        '
+                        ' PELLITAL_II ESTA MAL ESCRITA PORQUE LA BASE DE DATOS LO TIENE ASI AL NOMBRE.
+                        '
+                        Dim lista() = {"PellitalSA", "Pelitall_II", "Pellital_III", "Pellital_V"}
+                        Return New List(Of String)(lista)
+
+                    Case Else
+                        Dim lista() = {}
+                        Return New List(Of String)(lista)
+
+                End Select
+            End Get
+        End Property
+
+        Public Shared Function ConectarA(ByVal empresa As String) As String
+            'If String.IsNullOrEmpty(empresa) Then
+
+            '    If EmpresaDeTrabajo = "" Then Throw New Exception("Empresa no definida para realizar la conexión a la Base de Datos.")
+
+            '    empresa = EmpresaDeTrabajo
+
+            'End If
+
+            'If EsPellital AndAlso empresa = "SurfactanSA" Then
+            '    empresa = "Pellital_III"
+            'End If
+
+            'Dim _empresa = Nothing
+
+            '_empresa = DeterminarBasePara(empresa)
+
+            Return _ArmarCS("SurfactanSa")
+
+        End Function
+
+        Private Shared Function _ArmarCS(ByVal WEmpresa As String)
+
+            If Not String.IsNullOrEmpty(WEmpresa) Then
+
+                Dim cs = "Data Source=193.168.0.7;Initial Catalog=#EMPRESA#;User ID=usuarioadmin; Password=usuarioadmin" 'ConfigurationManager.ConnectionStrings("CS").ToString
+
+                Return cs.Replace("#EMPRESA#", WEmpresa)
+
+            Else
+                Throw New Exception("No se pudo encontrar la empresa a la que se quiere conectar.")
+            End If
+
+        End Function
+
+        Public Shared Function ConectarASegunID(ByVal empresa As Integer) As String
+
+            Dim _empresa = Nothing
+
+            _empresa = DeterminarSegunIDIDBasePara(empresa)
+
+            If Trim(_empresa) = "" Then Throw New Exception("No se pudo encontrar la empresa " & empresa)
+
+            Return _ArmarCS(_empresa)
+
+        End Function
+
+        Private Shared Function DeterminarSegunIDIDBasePara(ByVal empresa As Integer) As String
+
+            Select Case empresa
+                Case 1
+                    Return "SurfactanSa"
+                Case 2
+                    Return "PellitalSa"
+                Case 3
+                    Return "Surfactan_II"
+                Case 4
+                    Return "Pelitall_II" ' Asi está escrita la Base de Datos en el SQL.
+                Case 5
+                    Return "Surfactan_III"
+                Case 6
+                    Return "Surfactan_IV"
+                Case 7
+                    Return "Surfactan_V"
+                Case 8
+                    Return "Pellital_III"
+                Case 9
+                    Return "Pellital_V"
+                Case 10
+                    Return "Surfactan_VI"
+                Case 11
+                    Return "Surfactan_VII"
+                Case Else
+                    Return ""
+            End Select
+
+        End Function
+
+        Public Shared Function DeterminarBasePara(ByVal empresa As String) As String
+
+            Select Case UCase(empresa)
+                Case "SURFACTAN QUIMICOS"
+
+                    Return "Surfactan_II"
+
+                Case "SURFACTAN FARMA"
+
+                    Return "Surfactan_III"
+
+                Case "PELLITAL"
+
+                    Return "Pelitall_II" ' Se encuentra escrito asi el nombre en SQL.
+
+                Case "SURFACTAN PIGMENTOS"
+
+                    Return "SurfactanSA"
+                Case Else
+
+                    Return Empresas.Find(Function(e) UCase(e) = UCase(empresa))
+
+            End Select
+        End Function
+
+        'Public Shared Function _Login(ByVal Clave As String, ByVal WProceso As Integer) As Boolean
+
+        '    Dim cn As SqlConnection = New SqlConnection()
+        '    Dim cm As SqlCommand = New SqlCommand("SELECT Clave, Operador FROM Operador WHERE (Clave = '" & Clave & "' Or Clave = '" & UCase(Clave) & "')")
+        '    Dim dr As SqlDataReader
+        '    Dim WAtributos(10) As String
+
+        '    ' Inicializamos los valores de Atributos del Operador.
+        '    For i = 0 To 10
+        '        For j = 0 To 100
+        '            WAtributosOperador(i, j) = "0"
+        '        Next
+        '    Next
+
+        '    Try
+
+        '        cn.ConnectionString = Helper._ConectarA
+        '        cn.Open()
+        '        cm.Connection = cn
+
+        '        dr = cm.ExecuteReader()
+
+        '        If dr.HasRows Then
+        '            dr.Read()
+        '            Operador = IIf(IsDBNull(dr.Item("Operador")), "", dr.Item("Operador"))
+        '            dr.Close()
+
+        '            ' Armamos los datos de los Atributos.
+        '            cm.CommandText = "SELECT Atributo1, Atributo2, Atributo3, Atributo4, Atributo5, Atributo6, Atributo7, Atributo8, Atributo9, Atributo10 " _
+        '                            & " FROM Atributos WHERE Operador = '" & Operador & "' AND Proceso = '" & WProceso & "'"
+
+        '            dr = cm.ExecuteReader()
+
+        '            If dr.HasRows Then
+
+        '                dr.Read()
+
+        '                For i = 1 To 10
+
+        '                    WAtributos(i) = IIf(IsDBNull(dr.Item("Atributo" & i)), "", dr.Item("Atributo" & i))
+        '                    WAtributos(i) = WAtributos(i).PadRight(33, "0")
+
+        '                Next
+
+        '                ' Parseamos los valores de atributos.
+        '                Dim Auxiliar = ""
+        '                For Ciclo = 1 To 10
+        '                    Auxiliar = WAtributos(Ciclo)
+        '                    For Ciclo1 = 1 To 30
+        '                        WAtributosOperador(Ciclo, Ciclo1) = Val(Mid$(Auxiliar, Ciclo1, 1))
+        '                    Next Ciclo1
+        '                Next Ciclo
+
+        '            End If
+
+        '            Return True
+        '        Else
+        '            Operador = ""
+        '            Return False
+        '        End If
+
+        '    Catch ex As Exception
+        '        Throw New Exception("Hubo un problema al querer consultar la Base de Datos." & vbCrLf & vbCrLf & "Motivo: " & ex.Message)
+        '    Finally
+
+        '        dr = Nothing
+        '        cn.Close()
+        '        cn = Nothing
+        '        cm = Nothing
+
+        '    End Try
+
+        'End Function
+    End Class
+End Namespace
---
+++ LectoraControl/LectoraControl/Principal.Designer.vb
@@ -0,0 +1,289 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Public Class Principal
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Principal))
+        Me.txtCodigoCliente = New System.Windows.Forms.TextBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.txtCodigoGral = New System.Windows.Forms.TextBox
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.txtTotalPedido = New System.Windows.Forms.Label
+        Me.txtSuma = New System.Windows.Forms.Label
+        Me.btnLimpiar = New System.Windows.Forms.Button
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.txtCandidad = New System.Windows.Forms.TextBox
+        Me.pnlMsg = New System.Windows.Forms.Panel
+        Me.lblCuentaRegresiva = New System.Windows.Forms.Label
+        Me.picError = New System.Windows.Forms.PictureBox
+        Me.picExito = New System.Windows.Forms.PictureBox
+        Me.lblMensajeestado = New System.Windows.Forms.TextBox
+        Me.Timer1 = New System.Windows.Forms.Timer
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.txtCodProd = New System.Windows.Forms.TextBox
+        Me.txtDescProd = New System.Windows.Forms.TextBox
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.txtPartida = New System.Windows.Forms.TextBox
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.pnlMsg.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtCodigoCliente
+        '
+        Me.txtCodigoCliente.Location = New System.Drawing.Point(145, 131)
+        Me.txtCodigoCliente.Name = "txtCodigoCliente"
+        Me.txtCodigoCliente.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtCodigoCliente.Size = New System.Drawing.Size(70, 23)
+        Me.txtCodigoCliente.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(21, 133)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(126, 18)
+        Me.Label1.Text = "Etiqueta Cliente"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'txtCodigoGral
+        '
+        Me.txtCodigoGral.Location = New System.Drawing.Point(124, 19)
+        Me.txtCodigoGral.Name = "txtCodigoGral"
+        Me.txtCodigoGral.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtCodigoGral.Size = New System.Drawing.Size(91, 23)
+        Me.txtCodigoGral.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(17, 20)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(101, 18)
+        Me.Label2.Text = "Etiqueta General"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'txtTotalPedido
+        '
+        Me.txtTotalPedido.Location = New System.Drawing.Point(113, 243)
+        Me.txtTotalPedido.Name = "txtTotalPedido"
+        Me.txtTotalPedido.Size = New System.Drawing.Size(78, 19)
+        Me.txtTotalPedido.Text = "450 Kg(s)"
+        Me.txtTotalPedido.Visible = False
+        '
+        'txtSuma
+        '
+        Me.txtSuma.Location = New System.Drawing.Point(27, 165)
+        Me.txtSuma.Name = "txtSuma"
+        Me.txtSuma.Size = New System.Drawing.Size(187, 19)
+        Me.txtSuma.Text = "100"
+        Me.txtSuma.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'btnLimpiar
+        '
+        Me.btnLimpiar.Location = New System.Drawing.Point(17, 239)
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(28, 23)
+        Me.btnLimpiar.TabIndex = 7
+        Me.btnLimpiar.Text = "Limpiar"
+        Me.btnLimpiar.Visible = False
+        '
+        'Label5
+        '
+        Me.Label5.Location = New System.Drawing.Point(191, 243)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(14, 19)
+        Me.Label5.Text = "/"
+        Me.Label5.Visible = False
+        '
+        'txtCandidad
+        '
+        Me.txtCandidad.Location = New System.Drawing.Point(208, 243)
+        Me.txtCandidad.Name = "txtCandidad"
+        Me.txtCandidad.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtCandidad.Size = New System.Drawing.Size(27, 23)
+        Me.txtCandidad.TabIndex = 0
+        Me.txtCandidad.Visible = False
+        '
+        'pnlMsg
+        '
+        Me.pnlMsg.BackColor = System.Drawing.Color.White
+        Me.pnlMsg.Controls.Add(Me.lblCuentaRegresiva)
+        Me.pnlMsg.Controls.Add(Me.picError)
+        Me.pnlMsg.Controls.Add(Me.picExito)
+        Me.pnlMsg.Controls.Add(Me.lblMensajeestado)
+        Me.pnlMsg.Location = New System.Drawing.Point(63, 229)
+        Me.pnlMsg.Name = "pnlMsg"
+        Me.pnlMsg.Size = New System.Drawing.Size(44, 40)
+        '
+        'lblCuentaRegresiva
+        '
+        Me.lblCuentaRegresiva.Font = New System.Drawing.Font("Tahoma", 8.0!, System.Drawing.FontStyle.Regular)
+        Me.lblCuentaRegresiva.Location = New System.Drawing.Point(18, 207)
+        Me.lblCuentaRegresiva.Name = "lblCuentaRegresiva"
+        Me.lblCuentaRegresiva.Size = New System.Drawing.Size(169, 17)
+        Me.lblCuentaRegresiva.Text = "Mensaje se cierra en 1 seg(s)"
+        Me.lblCuentaRegresiva.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'picError
+        '
+        Me.picError.BackColor = System.Drawing.Color.Transparent
+        Me.picError.Image = CType(resources.GetObject("picError.Image"), System.Drawing.Image)
+        Me.picError.Location = New System.Drawing.Point(74, 10)
+        Me.picError.Name = "picError"
+        Me.picError.Size = New System.Drawing.Size(70, 66)
+        Me.picError.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picError.Visible = False
+        '
+        'picExito
+        '
+        Me.picExito.BackColor = System.Drawing.Color.Transparent
+        Me.picExito.Image = CType(resources.GetObject("picExito.Image"), System.Drawing.Image)
+        Me.picExito.Location = New System.Drawing.Point(73, 10)
+        Me.picExito.Name = "picExito"
+        Me.picExito.Size = New System.Drawing.Size(70, 66)
+        Me.picExito.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picExito.Visible = False
+        '
+        'lblMensajeestado
+        '
+        Me.lblMensajeestado.BackColor = System.Drawing.Color.White
+        Me.lblMensajeestado.Font = New System.Drawing.Font("Arial", 13.0!, System.Drawing.FontStyle.Bold)
+        Me.lblMensajeestado.Location = New System.Drawing.Point(14, 81)
+        Me.lblMensajeestado.Multiline = True
+        Me.lblMensajeestado.Name = "lblMensajeestado"
+        Me.lblMensajeestado.ReadOnly = True
+        Me.lblMensajeestado.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal
+        Me.lblMensajeestado.Size = New System.Drawing.Size(174, 122)
+        Me.lblMensajeestado.TabIndex = 3
+        Me.lblMensajeestado.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Label3
+        '
+        Me.Label3.Location = New System.Drawing.Point(13, 49)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(68, 18)
+        Me.Label3.Text = "Producto"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'txtCodProd
+        '
+        Me.txtCodProd.BackColor = System.Drawing.SystemColors.Control
+        Me.txtCodProd.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.txtCodProd.Enabled = False
+        Me.txtCodProd.Font = New System.Drawing.Font("Tahoma", 10.0!, System.Drawing.FontStyle.Bold)
+        Me.txtCodProd.Location = New System.Drawing.Point(87, 46)
+        Me.txtCodProd.Name = "txtCodProd"
+        Me.txtCodProd.ReadOnly = True
+        Me.txtCodProd.Size = New System.Drawing.Size(128, 23)
+        Me.txtCodProd.TabIndex = 0
+        '
+        'txtDescProd
+        '
+        Me.txtDescProd.BackColor = System.Drawing.SystemColors.Control
+        Me.txtDescProd.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.txtDescProd.Enabled = False
+        Me.txtDescProd.Location = New System.Drawing.Point(16, 98)
+        Me.txtDescProd.Name = "txtDescProd"
+        Me.txtDescProd.ReadOnly = True
+        Me.txtDescProd.Size = New System.Drawing.Size(198, 23)
+        Me.txtDescProd.TabIndex = 0
+        '
+        'Label4
+        '
+        Me.Label4.Location = New System.Drawing.Point(8, 74)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(68, 18)
+        Me.Label4.Text = "Partida"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'txtPartida
+        '
+        Me.txtPartida.BackColor = System.Drawing.SystemColors.Control
+        Me.txtPartida.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.txtPartida.Enabled = False
+        Me.txtPartida.Font = New System.Drawing.Font("Tahoma", 10.0!, System.Drawing.FontStyle.Bold)
+        Me.txtPartida.Location = New System.Drawing.Point(86, 71)
+        Me.txtPartida.Name = "txtPartida"
+        Me.txtPartida.ReadOnly = True
+        Me.txtPartida.Size = New System.Drawing.Size(128, 23)
+        Me.txtPartida.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(44, 187)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(152, 23)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "Reset"
+        '
+        'Principal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.AutoScroll = True
+        Me.ClientSize = New System.Drawing.Size(238, 269)
+        Me.Controls.Add(Me.pnlMsg)
+        Me.Controls.Add(Me.txtDescProd)
+        Me.Controls.Add(Me.txtPartida)
+        Me.Controls.Add(Me.txtCodProd)
+        Me.Controls.Add(Me.txtCodigoGral)
+        Me.Controls.Add(Me.txtCandidad)
+        Me.Controls.Add(Me.txtCodigoCliente)
+        Me.Controls.Add(Me.txtTotalPedido)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnLimpiar)
+        Me.Controls.Add(Me.txtSuma)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Principal"
+        Me.Text = "Form1"
+        Me.pnlMsg.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents txtCodigoCliente As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtCodigoGral As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtTotalPedido As System.Windows.Forms.Label
+    Friend WithEvents txtSuma As System.Windows.Forms.Label
+    Friend WithEvents btnLimpiar As System.Windows.Forms.Button
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtCandidad As System.Windows.Forms.TextBox
+    Friend WithEvents pnlMsg As System.Windows.Forms.Panel
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents lblMensajeestado As System.Windows.Forms.TextBox
+    Friend WithEvents picExito As System.Windows.Forms.PictureBox
+    Friend WithEvents picError As System.Windows.Forms.PictureBox
+    Friend WithEvents lblCuentaRegresiva As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtCodProd As System.Windows.Forms.TextBox
+    Friend WithEvents txtDescProd As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtPartida As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
