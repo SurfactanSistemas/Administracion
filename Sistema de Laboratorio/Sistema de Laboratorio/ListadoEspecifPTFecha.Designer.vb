64130b1e40936ddb5e3e48266caf9c38a599d8c9
Ferreyra, M. Gaston <fergthh@gmail.com>
2020-04-07 17:31:56 -0300

    Sist. de Lab: REFACTOR - Se ajusta visualmente y se modifican procesos en Listado de Especificaciones de Pt a Fecha.

--- Sistema de Laboratorio/Sistema de Laboratorio/ListadoEspecifPTFecha.Designer.vb
+++ Sistema de Laboratorio/Sistema de Laboratorio/ListadoEspecifPTFecha.Designer.vb
@@ -42,118 +42,122 @@ Partial Class ListadoEspecifPTFecha
         '
         'mastxtFechaDesde
         '
-        Me.mastxtFechaDesde.Location = New System.Drawing.Point(133, 82)
+        Me.mastxtFechaDesde.Location = New System.Drawing.Point(63, 56)
         Me.mastxtFechaDesde.Mask = "00/00/0000"
         Me.mastxtFechaDesde.Name = "mastxtFechaDesde"
         Me.mastxtFechaDesde.PromptChar = Global.Microsoft.VisualBasic.ChrW(32)
-        Me.mastxtFechaDesde.Size = New System.Drawing.Size(100, 20)
+        Me.mastxtFechaDesde.Size = New System.Drawing.Size(71, 20)
         Me.mastxtFechaDesde.TabIndex = 0
+        Me.mastxtFechaDesde.Text = "99999999"
         Me.mastxtFechaDesde.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
         '
         'mastxtFechaHasta
         '
-        Me.mastxtFechaHasta.Location = New System.Drawing.Point(133, 129)
+        Me.mastxtFechaHasta.Location = New System.Drawing.Point(191, 56)
         Me.mastxtFechaHasta.Mask = "00/00/0000"
         Me.mastxtFechaHasta.Name = "mastxtFechaHasta"
         Me.mastxtFechaHasta.PromptChar = Global.Microsoft.VisualBasic.ChrW(32)
-        Me.mastxtFechaHasta.Size = New System.Drawing.Size(100, 20)
+        Me.mastxtFechaHasta.Size = New System.Drawing.Size(72, 20)
         Me.mastxtFechaHasta.TabIndex = 1
+        Me.mastxtFechaHasta.Text = "99999999"
         Me.mastxtFechaHasta.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
         '
         'mastxtDePT
         '
-        Me.mastxtDePT.Location = New System.Drawing.Point(133, 177)
+        Me.mastxtDePT.Location = New System.Drawing.Point(62, 87)
         Me.mastxtDePT.Mask = ">LL-00000-000"
         Me.mastxtDePT.Name = "mastxtDePT"
         Me.mastxtDePT.PromptChar = Global.Microsoft.VisualBasic.ChrW(32)
-        Me.mastxtDePT.Size = New System.Drawing.Size(100, 20)
+        Me.mastxtDePT.Size = New System.Drawing.Size(80, 20)
         Me.mastxtDePT.TabIndex = 2
+        Me.mastxtDePT.Text = "PT99999999"
         Me.mastxtDePT.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
         '
         'mastxtAPT
         '
-        Me.mastxtAPT.Location = New System.Drawing.Point(133, 227)
+        Me.mastxtAPT.Location = New System.Drawing.Point(197, 87)
         Me.mastxtAPT.Mask = ">LL-00000-000"
         Me.mastxtAPT.Name = "mastxtAPT"
         Me.mastxtAPT.PromptChar = Global.Microsoft.VisualBasic.ChrW(32)
-        Me.mastxtAPT.Size = New System.Drawing.Size(100, 20)
+        Me.mastxtAPT.Size = New System.Drawing.Size(81, 20)
         Me.mastxtAPT.TabIndex = 3
+        Me.mastxtAPT.Text = "PT99999999"
         Me.mastxtAPT.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(73, 85)
+        Me.Label1.Location = New System.Drawing.Point(12, 60)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
         Me.Label1.TabIndex = 4
-        Me.Label1.Text = "Desde"
+        Me.Label1.Text = "DESDE"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(73, 132)
+        Me.Label2.Location = New System.Drawing.Point(141, 60)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.Size = New System.Drawing.Size(43, 13)
         Me.Label2.TabIndex = 5
-        Me.Label2.Text = "Hasta"
+        Me.Label2.Text = "HASTA"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(7, 180)
+        Me.Label3.Location = New System.Drawing.Point(12, 90)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(120, 13)
+        Me.Label3.Size = New System.Drawing.Size(44, 13)
         Me.Label3.TabIndex = 6
-        Me.Label3.Text = "De Producto Terminado"
+        Me.Label3.Text = "DESDE"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(7, 230)
+        Me.Label4.Location = New System.Drawing.Point(148, 90)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(113, 13)
+        Me.Label4.Size = New System.Drawing.Size(43, 13)
         Me.Label4.TabIndex = 7
-        Me.Label4.Text = "A Producto Terminado"
+        Me.Label4.Text = "HASTA"
         '
         'btnAceptar
         '
-        Me.btnAceptar.Location = New System.Drawing.Point(261, 196)
+        Me.btnAceptar.Location = New System.Drawing.Point(98, 124)
         Me.btnAceptar.Name = "btnAceptar"
-        Me.btnAceptar.Size = New System.Drawing.Size(75, 23)
+        Me.btnAceptar.Size = New System.Drawing.Size(121, 51)
         Me.btnAceptar.TabIndex = 8
-        Me.btnAceptar.Text = "Aceptar"
+        Me.btnAceptar.Text = "ACEPTAR"
         Me.btnAceptar.UseVisualStyleBackColor = True
         '
         'btnVolver
         '
-        Me.btnVolver.Location = New System.Drawing.Point(358, 196)
+        Me.btnVolver.Location = New System.Drawing.Point(225, 124)
         Me.btnVolver.Name = "btnVolver"
-        Me.btnVolver.Size = New System.Drawing.Size(75, 23)
+        Me.btnVolver.Size = New System.Drawing.Size(121, 51)
         Me.btnVolver.TabIndex = 9
-        Me.btnVolver.Text = "Volver"
+        Me.btnVolver.Text = "CERRAR"
         Me.btnVolver.UseVisualStyleBackColor = True
         '
         'rabPantalla
         '
         Me.rabPantalla.AutoSize = True
         Me.rabPantalla.Checked = True
-        Me.rabPantalla.Location = New System.Drawing.Point(310, 122)
+        Me.rabPantalla.Location = New System.Drawing.Point(270, 58)
         Me.rabPantalla.Name = "rabPantalla"
-        Me.rabPantalla.Size = New System.Drawing.Size(63, 17)
+        Me.rabPantalla.Size = New System.Drawing.Size(80, 17)
         Me.rabPantalla.TabIndex = 10
         Me.rabPantalla.TabStop = True
-        Me.rabPantalla.Text = "Pantalla"
+        Me.rabPantalla.Text = "PANTALLA"
         Me.rabPantalla.UseVisualStyleBackColor = True
         '
         'rabImprimir
         '
         Me.rabImprimir.AutoSize = True
-        Me.rabImprimir.Location = New System.Drawing.Point(310, 163)
+        Me.rabImprimir.Location = New System.Drawing.Point(357, 58)
         Me.rabImprimir.Name = "rabImprimir"
-        Me.rabImprimir.Size = New System.Drawing.Size(60, 17)
+        Me.rabImprimir.Size = New System.Drawing.Size(75, 17)
         Me.rabImprimir.TabIndex = 11
-        Me.rabImprimir.Text = "Imprimir"
+        Me.rabImprimir.Text = "IMPRIMIR"
         Me.rabImprimir.UseVisualStyleBackColor = True
         '
         'Panel1
@@ -161,9 +165,10 @@ Partial Class ListadoEspecifPTFecha
         Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(32, Byte), Integer), CType(CType(74, Byte), Integer), CType(CType(95, Byte), Integer))
         Me.Panel1.Controls.Add(Me.Label6)
         Me.Panel1.Controls.Add(Me.Label5)
-        Me.Panel1.Location = New System.Drawing.Point(-19, -30)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(612, 100)
+        Me.Panel1.Size = New System.Drawing.Size(445, 46)
         Me.Panel1.TabIndex = 12
         '
         'Label6
@@ -171,13 +176,13 @@ Partial Class ListadoEspecifPTFecha
         Me.Label6.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Label6.AutoSize = True
-        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label6.ForeColor = System.Drawing.SystemColors.Control
-        Me.Label6.Location = New System.Drawing.Point(21, 39)
+        Me.Label6.Location = New System.Drawing.Point(12, 14)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(398, 20)
+        Me.Label6.Size = New System.Drawing.Size(213, 18)
         Me.Label6.TabIndex = 14
-        Me.Label6.Text = "Listado de Especif de Prod. Terminados a Fecha"
+        Me.Label6.Text = "Listado Especif. de PT a Fecha"
         '
         'Label5
         '
@@ -186,7 +191,7 @@ Partial Class ListadoEspecifPTFecha
         Me.Label5.AutoSize = True
         Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label5.ForeColor = System.Drawing.SystemColors.Control
-        Me.Label5.Location = New System.Drawing.Point(273, 67)
+        Me.Label5.Location = New System.Drawing.Point(254, 11)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(179, 24)
         Me.Label5.TabIndex = 13
@@ -196,7 +201,7 @@ Partial Class ListadoEspecifPTFecha
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(445, 265)
+        Me.ClientSize = New System.Drawing.Size(445, 186)
         Me.Controls.Add(Me.rabImprimir)
         Me.Controls.Add(Me.rabPantalla)
         Me.Controls.Add(Me.btnVolver)
@@ -210,8 +215,11 @@ Partial Class ListadoEspecifPTFecha
         Me.Controls.Add(Me.mastxtFechaHasta)
         Me.Controls.Add(Me.mastxtFechaDesde)
         Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
         Me.Name = "ListadoEspecifPTFecha"
-        Me.Text = "ListadoEspecifPTFecha"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
