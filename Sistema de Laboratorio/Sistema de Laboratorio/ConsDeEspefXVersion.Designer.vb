b515ec461acb98db0e2171ac2b218274e661b0ee
Ferreyra, M. Gaston <fergthh@gmail.com>
2020-04-07 17:16:37 -0300

    Sist. de Lab: REFACTOR - Se ajusta visualmente ventana de Especificaciones por Version de PT.

--- Sistema de Laboratorio/Sistema de Laboratorio/ConsDeEspefXVersion.Designer.vb
+++ Sistema de Laboratorio/Sistema de Laboratorio/ConsDeEspefXVersion.Designer.vb
@@ -22,7 +22,7 @@ Partial Class ConsDeEspefXVersion
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Me.mastxtCodigo = New System.Windows.Forms.MaskedTextBox()
         Me.txtVersion = New System.Windows.Forms.TextBox()
         Me.txtFechaDesde = New System.Windows.Forms.TextBox()
@@ -46,23 +46,25 @@ Partial Class ConsDeEspefXVersion
         Me.Panel1 = New System.Windows.Forms.Panel()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtDescTerminado = New System.Windows.Forms.TextBox()
         CType(Me.DGV_ConsultaVersiones, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'mastxtCodigo
         '
-        Me.mastxtCodigo.Location = New System.Drawing.Point(62, 54)
+        Me.mastxtCodigo.Location = New System.Drawing.Point(88, 54)
         Me.mastxtCodigo.Mask = ">LL-00000-000"
         Me.mastxtCodigo.Name = "mastxtCodigo"
         Me.mastxtCodigo.PromptChar = Global.Microsoft.VisualBasic.ChrW(32)
-        Me.mastxtCodigo.Size = New System.Drawing.Size(82, 20)
+        Me.mastxtCodigo.Size = New System.Drawing.Size(78, 20)
         Me.mastxtCodigo.TabIndex = 0
+        Me.mastxtCodigo.Text = "PT99999999"
         Me.mastxtCodigo.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
         '
         'txtVersion
         '
-        Me.txtVersion.Location = New System.Drawing.Point(263, 54)
+        Me.txtVersion.Location = New System.Drawing.Point(88, 82)
         Me.txtVersion.MaxLength = 2
         Me.txtVersion.Name = "txtVersion"
         Me.txtVersion.Size = New System.Drawing.Size(57, 20)
@@ -72,7 +74,7 @@ Partial Class ConsDeEspefXVersion
         '
         Me.txtFechaDesde.BackColor = System.Drawing.Color.Cyan
         Me.txtFechaDesde.Enabled = False
-        Me.txtFechaDesde.Location = New System.Drawing.Point(602, 54)
+        Me.txtFechaDesde.Location = New System.Drawing.Point(423, 82)
         Me.txtFechaDesde.Name = "txtFechaDesde"
         Me.txtFechaDesde.Size = New System.Drawing.Size(79, 20)
         Me.txtFechaDesde.TabIndex = 2
@@ -81,99 +83,100 @@ Partial Class ConsDeEspefXVersion
         '
         Me.txtFechaHasta.BackColor = System.Drawing.Color.Cyan
         Me.txtFechaHasta.Enabled = False
-        Me.txtFechaHasta.Location = New System.Drawing.Point(687, 54)
+        Me.txtFechaHasta.Location = New System.Drawing.Point(513, 82)
         Me.txtFechaHasta.Name = "txtFechaHasta"
         Me.txtFechaHasta.Size = New System.Drawing.Size(79, 20)
         Me.txtFechaHasta.TabIndex = 3
         '
         'btnAnteriorVersion
         '
-        Me.btnAnteriorVersion.Location = New System.Drawing.Point(337, 52)
+        Me.btnAnteriorVersion.Location = New System.Drawing.Point(171, 79)
         Me.btnAnteriorVersion.Name = "btnAnteriorVersion"
-        Me.btnAnteriorVersion.Size = New System.Drawing.Size(59, 22)
+        Me.btnAnteriorVersion.Size = New System.Drawing.Size(97, 27)
         Me.btnAnteriorVersion.TabIndex = 4
-        Me.btnAnteriorVersion.Text = "Anterior"
+        Me.btnAnteriorVersion.Text = "ANTERIOR"
         Me.btnAnteriorVersion.UseVisualStyleBackColor = True
         '
         'btnSiguienteVersion
         '
-        Me.btnSiguienteVersion.Location = New System.Drawing.Point(402, 52)
+        Me.btnSiguienteVersion.Location = New System.Drawing.Point(272, 79)
         Me.btnSiguienteVersion.Name = "btnSiguienteVersion"
-        Me.btnSiguienteVersion.Size = New System.Drawing.Size(59, 22)
+        Me.btnSiguienteVersion.Size = New System.Drawing.Size(97, 27)
         Me.btnSiguienteVersion.TabIndex = 5
-        Me.btnSiguienteVersion.Text = "Siguiente"
+        Me.btnSiguienteVersion.Text = "SIGUIENTE"
         Me.btnSiguienteVersion.UseVisualStyleBackColor = True
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(19, 57)
+        Me.Label1.Location = New System.Drawing.Point(28, 57)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.Size = New System.Drawing.Size(49, 13)
         Me.Label1.TabIndex = 6
-        Me.Label1.Text = "Codigo"
+        Me.Label1.Text = "CODIGO"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(212, 57)
+        Me.Label2.Location = New System.Drawing.Point(28, 86)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(42, 13)
+        Me.Label2.Size = New System.Drawing.Size(55, 13)
         Me.Label2.TabIndex = 7
-        Me.Label2.Text = "Version"
+        Me.Label2.Text = "VERSION"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(559, 57)
+        Me.Label3.Location = New System.Drawing.Point(375, 86)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(37, 13)
+        Me.Label3.Size = New System.Drawing.Size(42, 13)
         Me.Label3.TabIndex = 8
-        Me.Label3.Text = "Fecha"
+        Me.Label3.Text = "FECHA"
         '
         'btnLimpiar
         '
-        Me.btnLimpiar.Location = New System.Drawing.Point(532, 388)
+        Me.btnLimpiar.Location = New System.Drawing.Point(508, 417)
         Me.btnLimpiar.Name = "btnLimpiar"
-        Me.btnLimpiar.Size = New System.Drawing.Size(75, 23)
+        Me.btnLimpiar.Size = New System.Drawing.Size(101, 46)
         Me.btnLimpiar.TabIndex = 9
-        Me.btnLimpiar.Text = "Limpiar"
+        Me.btnLimpiar.Text = "LIMPIAR"
         Me.btnLimpiar.UseVisualStyleBackColor = True
         '
         'btnImprimir
         '
-        Me.btnImprimir.Location = New System.Drawing.Point(641, 388)
+        Me.btnImprimir.Location = New System.Drawing.Point(615, 417)
         Me.btnImprimir.Name = "btnImprimir"
-        Me.btnImprimir.Size = New System.Drawing.Size(75, 23)
+        Me.btnImprimir.Size = New System.Drawing.Size(101, 46)
         Me.btnImprimir.TabIndex = 10
-        Me.btnImprimir.Text = "Imprimir"
+        Me.btnImprimir.Text = "IMPRIMIR"
         Me.btnImprimir.UseVisualStyleBackColor = True
         '
         'btnVolver
         '
-        Me.btnVolver.Location = New System.Drawing.Point(745, 388)
+        Me.btnVolver.Location = New System.Drawing.Point(722, 417)
         Me.btnVolver.Name = "btnVolver"
-        Me.btnVolver.Size = New System.Drawing.Size(75, 23)
+        Me.btnVolver.Size = New System.Drawing.Size(101, 46)
         Me.btnVolver.TabIndex = 11
-        Me.btnVolver.Text = "Volver"
+        Me.btnVolver.Text = "CERRAR"
         Me.btnVolver.UseVisualStyleBackColor = True
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(15, 393)
+        Me.Label4.Location = New System.Drawing.Point(15, 421)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(98, 13)
+        Me.Label4.Size = New System.Drawing.Size(128, 13)
         Me.Label4.TabIndex = 12
-        Me.Label4.Text = "Control de Cambios"
+        Me.Label4.Text = "CONTROL DE CAMBIOS"
         '
         'txtControlDeCambios
         '
         Me.txtControlDeCambios.BackColor = System.Drawing.Color.Cyan
         Me.txtControlDeCambios.Enabled = False
-        Me.txtControlDeCambios.Location = New System.Drawing.Point(117, 390)
+        Me.txtControlDeCambios.Location = New System.Drawing.Point(147, 417)
+        Me.txtControlDeCambios.Multiline = True
         Me.txtControlDeCambios.Name = "txtControlDeCambios"
-        Me.txtControlDeCambios.Size = New System.Drawing.Size(386, 20)
+        Me.txtControlDeCambios.Size = New System.Drawing.Size(355, 46)
         Me.txtControlDeCambios.TabIndex = 13
         '
         'DGV_ConsultaVersiones
@@ -181,17 +184,17 @@ Partial Class ConsDeEspefXVersion
         Me.DGV_ConsultaVersiones.AllowUserToAddRows = False
         Me.DGV_ConsultaVersiones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DGV_ConsultaVersiones.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Ensayo, Me.Descripcion, Me.ValorEstandar, Me.Desde, Me.Hasta})
-        DataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
-        DataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(CType(CType(12, Byte), Integer), CType(CType(217, Byte), Integer), CType(CType(232, Byte), Integer))
-        DataGridViewCellStyle2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        DataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText
-        DataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(170, Byte), Integer), CType(CType(255, Byte), Integer))
-        DataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White
-        DataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.[False]
-        Me.DGV_ConsultaVersiones.DefaultCellStyle = DataGridViewCellStyle2
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(CType(CType(12, Byte), Integer), CType(CType(217, Byte), Integer), CType(CType(232, Byte), Integer))
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(170, Byte), Integer), CType(CType(255, Byte), Integer))
+        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.White
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.DGV_ConsultaVersiones.DefaultCellStyle = DataGridViewCellStyle1
         Me.DGV_ConsultaVersiones.DoubleBuffered = True
         Me.DGV_ConsultaVersiones.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically
-        Me.DGV_ConsultaVersiones.Location = New System.Drawing.Point(13, 83)
+        Me.DGV_ConsultaVersiones.Location = New System.Drawing.Point(13, 112)
         Me.DGV_ConsultaVersiones.Name = "DGV_ConsultaVersiones"
         Me.DGV_ConsultaVersiones.OrdenamientoColumnasHabilitado = True
         Me.DGV_ConsultaVersiones.RowHeadersWidth = 15
@@ -241,9 +244,10 @@ Partial Class ConsDeEspefXVersion
         Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(32, Byte), Integer), CType(CType(74, Byte), Integer), CType(CType(95, Byte), Integer))
         Me.Panel1.Controls.Add(Me.Label6)
         Me.Panel1.Controls.Add(Me.Label5)
-        Me.Panel1.Location = New System.Drawing.Point(-5, -5)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(952, 53)
+        Me.Panel1.Size = New System.Drawing.Size(839, 47)
         Me.Panel1.TabIndex = 15
         '
         'Label6
@@ -251,11 +255,11 @@ Partial Class ConsDeEspefXVersion
         Me.Label6.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Label6.AutoSize = True
-        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label6.ForeColor = System.Drawing.SystemColors.Control
-        Me.Label6.Location = New System.Drawing.Point(14, 14)
+        Me.Label6.Location = New System.Drawing.Point(23, 14)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(342, 20)
+        Me.Label6.Size = New System.Drawing.Size(283, 18)
         Me.Label6.TabIndex = 2
         Me.Label6.Text = "Consulta de Especificaciones por Version"
         '
@@ -264,20 +268,30 @@ Partial Class ConsDeEspefXVersion
         Me.Label5.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Label5.AutoSize = True
-        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label5.ForeColor = System.Drawing.SystemColors.Control
-        Me.Label5.Location = New System.Drawing.Point(651, 14)
+        Me.Label5.Location = New System.Drawing.Point(660, 14)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(179, 24)
+        Me.Label5.Size = New System.Drawing.Size(155, 20)
         Me.Label5.TabIndex = 1
         Me.Label5.Text = "SURFACTAN S.A."
+        '
+        'txtDescTerminado
+        '
+        Me.txtDescTerminado.BackColor = System.Drawing.Color.Cyan
+        Me.txtDescTerminado.Enabled = False
+        Me.txtDescTerminado.Location = New System.Drawing.Point(172, 53)
+        Me.txtDescTerminado.Name = "txtDescTerminado"
+        Me.txtDescTerminado.Size = New System.Drawing.Size(633, 20)
+        Me.txtDescTerminado.TabIndex = 13
         '
         'ConsDeEspefXVersion
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(832, 418)
+        Me.ClientSize = New System.Drawing.Size(839, 475)
         Me.Controls.Add(Me.DGV_ConsultaVersiones)
+        Me.Controls.Add(Me.txtDescTerminado)
         Me.Controls.Add(Me.txtControlDeCambios)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.btnVolver)
@@ -293,8 +307,10 @@ Partial Class ConsDeEspefXVersion
         Me.Controls.Add(Me.txtVersion)
         Me.Controls.Add(Me.mastxtCodigo)
         Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
         Me.Name = "ConsDeEspefXVersion"
-        Me.Text = "ConsDeEspefXVersion"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         CType(Me.DGV_ConsultaVersiones, System.ComponentModel.ISupportInitialize).EndInit()
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
@@ -325,4 +341,5 @@ Partial Class ConsDeEspefXVersion
     Friend WithEvents ValorEstandar As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents Desde As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents Hasta As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents txtDescTerminado As System.Windows.Forms.TextBox
 End Class
