0110a3e572a456a417f7eed5ec54c030436a29f1
Maximiliano Gastón Ferreyra <fergthh@gmail.com>
2020-08-21 16:06:06 -0300

    Gaston: 21 08 2020

---
+++ Administracion/EvaluacionProvMPFarma/VencimientosProximosEvaluaciones.Designer.vb
@@ -0,0 +1,308 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class VencimientosProximosEvaluaciones
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle7 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle8 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle5 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle6 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.panel1 = New System.Windows.Forms.Panel()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnBuscar = New System.Windows.Forms.Button()
+        Me.txtHasta = New System.Windows.Forms.MaskedTextBox()
+        Me.txtDesde = New System.Windows.Forms.MaskedTextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.dgvItems = New Util.DBDataGridView()
+        Me.Proveedor = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Nombre = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Articulo = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Descripcion = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Fecha = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.FechaOrd = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Item = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DescItem = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.panel1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.dgvItems, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'panel1
+        '
+        Me.panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(34, Byte), Integer), CType(CType(74, Byte), Integer), CType(CType(95, Byte), Integer))
+        Me.panel1.Controls.Add(Me.label1)
+        Me.panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.panel1.Location = New System.Drawing.Point(0, 0)
+        Me.panel1.Name = "panel1"
+        Me.panel1.Size = New System.Drawing.Size(892, 40)
+        Me.panel1.TabIndex = 3
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label1.ForeColor = System.Drawing.Color.White
+        Me.label1.Location = New System.Drawing.Point(8, 11)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(505, 19)
+        Me.label1.TabIndex = 0
+        Me.label1.Text = "VENCIMIENTOS PRÓXIMOS DE ITEMS DE EVAL DE PROVEEDORES DE FARMA"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox1.Controls.Add(Me.btnBuscar)
+        Me.GroupBox1.Controls.Add(Me.txtHasta)
+        Me.GroupBox1.Controls.Add(Me.txtDesde)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(6, 42)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(881, 87)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.Location = New System.Drawing.Point(313, 18)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(101, 34)
+        Me.btnBuscar.TabIndex = 3
+        Me.btnBuscar.Text = "BUSCAR"
+        Me.btnBuscar.UseVisualStyleBackColor = True
+        '
+        'txtHasta
+        '
+        Me.txtHasta.Location = New System.Drawing.Point(225, 25)
+        Me.txtHasta.Mask = "00/00/0000"
+        Me.txtHasta.Name = "txtHasta"
+        Me.txtHasta.PromptChar = Global.Microsoft.VisualBasic.ChrW(32)
+        Me.txtHasta.Size = New System.Drawing.Size(72, 20)
+        Me.txtHasta.TabIndex = 2
+        Me.txtHasta.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'txtDesde
+        '
+        Me.txtDesde.Location = New System.Drawing.Point(78, 25)
+        Me.txtDesde.Mask = "00/00/0000"
+        Me.txtDesde.Name = "txtDesde"
+        Me.txtDesde.PromptChar = Global.Microsoft.VisualBasic.ChrW(32)
+        Me.txtDesde.Size = New System.Drawing.Size(72, 20)
+        Me.txtDesde.TabIndex = 2
+        Me.txtDesde.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(76, 58)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(795, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(166, 29)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(43, 13)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "HASTA"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(18, 29)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(44, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "DESDE"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(18, 62)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "FILTRAR"
+        '
+        'dgvItems
+        '
+        Me.dgvItems.AllowUserToAddRows = False
+        Me.dgvItems.AllowUserToDeleteRows = False
+        Me.dgvItems.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dgvItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvItems.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Proveedor, Me.Nombre, Me.Articulo, Me.Descripcion, Me.Fecha, Me.FechaOrd, Me.Item, Me.DescItem})
+        DataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle7.BackColor = System.Drawing.Color.FromArgb(CType(CType(12, Byte), Integer), CType(CType(217, Byte), Integer), CType(CType(232, Byte), Integer))
+        DataGridViewCellStyle7.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle7.ForeColor = System.Drawing.SystemColors.ControlText
+        DataGridViewCellStyle7.SelectionBackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(170, Byte), Integer), CType(CType(255, Byte), Integer))
+        DataGridViewCellStyle7.SelectionForeColor = System.Drawing.Color.White
+        DataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.dgvItems.DefaultCellStyle = DataGridViewCellStyle7
+        Me.dgvItems.DoubleBuffered = True
+        Me.dgvItems.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter
+        Me.dgvItems.Location = New System.Drawing.Point(6, 135)
+        Me.dgvItems.Name = "dgvItems"
+        Me.dgvItems.OrdenamientoColumnasHabilitado = False
+        Me.dgvItems.ReadOnly = True
+        Me.dgvItems.RowHeadersWidth = 15
+        DataGridViewCellStyle8.Padding = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.dgvItems.RowsDefaultCellStyle = DataGridViewCellStyle8
+        Me.dgvItems.RowTemplate.Height = 20
+        Me.dgvItems.ShowCellToolTips = False
+        Me.dgvItems.SinClickDerecho = False
+        Me.dgvItems.Size = New System.Drawing.Size(881, 348)
+        Me.dgvItems.TabIndex = 4
+        '
+        'Proveedor
+        '
+        Me.Proveedor.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells
+        Me.Proveedor.DataPropertyName = "Proveedor"
+        DataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight
+        DataGridViewCellStyle5.NullValue = "0"
+        Me.Proveedor.DefaultCellStyle = DataGridViewCellStyle5
+        Me.Proveedor.HeaderText = "Proveedor"
+        Me.Proveedor.Name = "Proveedor"
+        Me.Proveedor.ReadOnly = True
+        Me.Proveedor.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable
+        Me.Proveedor.Width = 62
+        '
+        'Nombre
+        '
+        Me.Nombre.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None
+        Me.Nombre.DataPropertyName = "Nombre"
+        Me.Nombre.HeaderText = "Nombre"
+        Me.Nombre.MinimumWidth = 150
+        Me.Nombre.Name = "Nombre"
+        Me.Nombre.ReadOnly = True
+        Me.Nombre.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable
+        Me.Nombre.Width = 150
+        '
+        'Articulo
+        '
+        Me.Articulo.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells
+        Me.Articulo.DataPropertyName = "Articulo"
+        Me.Articulo.HeaderText = "Articulo"
+        Me.Articulo.Name = "Articulo"
+        Me.Articulo.ReadOnly = True
+        Me.Articulo.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable
+        Me.Articulo.Width = 48
+        '
+        'Descripcion
+        '
+        Me.Descripcion.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None
+        Me.Descripcion.DataPropertyName = "Descripcion"
+        Me.Descripcion.HeaderText = "Descripción"
+        Me.Descripcion.MinimumWidth = 150
+        Me.Descripcion.Name = "Descripcion"
+        Me.Descripcion.ReadOnly = True
+        Me.Descripcion.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable
+        Me.Descripcion.Width = 150
+        '
+        'Fecha
+        '
+        Me.Fecha.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells
+        Me.Fecha.DataPropertyName = "Fecha"
+        Me.Fecha.HeaderText = "Fecha"
+        Me.Fecha.Name = "Fecha"
+        Me.Fecha.ReadOnly = True
+        Me.Fecha.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable
+        Me.Fecha.Width = 43
+        '
+        'FechaOrd
+        '
+        Me.FechaOrd.DataPropertyName = "FechaOrd"
+        Me.FechaOrd.HeaderText = "FechaOrd"
+        Me.FechaOrd.Name = "FechaOrd"
+        Me.FechaOrd.ReadOnly = True
+        Me.FechaOrd.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable
+        Me.FechaOrd.Visible = False
+        '
+        'Item
+        '
+        Me.Item.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells
+        Me.Item.DataPropertyName = "Item"
+        DataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight
+        Me.Item.DefaultCellStyle = DataGridViewCellStyle6
+        Me.Item.HeaderText = "Item"
+        Me.Item.Name = "Item"
+        Me.Item.ReadOnly = True
+        Me.Item.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable
+        Me.Item.Width = 33
+        '
+        'DescItem
+        '
+        Me.DescItem.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill
+        Me.DescItem.DataPropertyName = "DescItem"
+        Me.DescItem.HeaderText = "Descripción"
+        Me.DescItem.Name = "DescItem"
+        Me.DescItem.ReadOnly = True
+        Me.DescItem.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable
+        '
+        'VencimientosProximosEvaluaciones
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(892, 495)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.dgvItems)
+        Me.Controls.Add(Me.panel1)
+        Me.Name = "VencimientosProximosEvaluaciones"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.panel1.ResumeLayout(False)
+        Me.panel1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.dgvItems, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Private WithEvents panel1 As System.Windows.Forms.Panel
+    Private WithEvents label1 As System.Windows.Forms.Label
+    Friend WithEvents dgvItems As Util.DBDataGridView
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Proveedor As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Nombre As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Articulo As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Descripcion As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Fecha As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents FechaOrd As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Item As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DescItem As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents txtHasta As System.Windows.Forms.MaskedTextBox
+    Friend WithEvents txtDesde As System.Windows.Forms.MaskedTextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents btnBuscar As System.Windows.Forms.Button
+End Class
---
+++ Administracion/EvaluacionProvMPFarma/VencimientosProximosEvaluaciones.vb
@@ -0,0 +1,106 @@
+﻿Imports Util.Clases.Query
+Imports Util.Clases.Helper
+
+Public Class VencimientosProximosEvaluaciones
+
+    Private dir As DireccionOrdenamiento = DireccionOrdenamiento.ASC
+
+    Private Sub VencimientosProximosEvaluaciones_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        txtDesde.Text = Date.Now.AddDays(-7).ToString("dd/MM/yyyy")
+        txtHasta.Text = Date.Now.AddMonths(2).ToString("dd/MM/yyyy")
+
+        _CargarDatos()
+
+    End Sub
+
+    Private Sub _CargarDatos()
+        Dim WFechaInicialOrd As String = Helper.ordenaFecha(txtDesde.Text)
+        Dim WFechaFinalOrd As String = Helper.ordenaFecha(txtHasta.Text)
+
+        Dim WDatos As DataTable = GetAll("select e.Proveedor, p.Nombre, e.Articulo, a.Descripcion, e.FechaVto as Fecha, e.FechaVtoOrd as FechaOrd, e.Renglon as Item, i.Descripcion DescItem from EvaluacionProvMP e INNER JOIN Proveedor p ON p.Proveedor = e.Proveedor INNER JOIN EvaluacionProvMPItems i on i.ID = e.Renglon INNER JOIN Articulo a ON a.Codigo = e.Articulo where LTRIM(replace(e.FechaVto, '/', '')) <> '' and e.fechavtoord BETWEEN '" & WFechaInicialOrd & "' and '" & WFechaFinalOrd & "' order by e.fechavtoord, e.Proveedor, p.Nombre")
+
+        For Each row As DataRow In GetAll("select e.Proveedor, p.Nombre, e.Articulo, a.Descripcion, e.FechaEvaluaVto as Fecha, e.FechaEvaluaVtoOrd As FechaOrd, 0 AS Item, 'EVALUACION GENERAL' As DescItem from EvaluacionProvMP e INNER JOIN Proveedor p ON p.Proveedor = e.Proveedor INNER JOIN Articulo a ON a.Codigo = e.Articulo where LTRIM(replace(FechaEvaluaVto, '/', '')) <> '' and renglon = 1 and e.FechaEvaluaVtoOrd BETWEEN '" & WFechaInicialOrd & "' and '" & WFechaFinalOrd & "' order by e.FechaEvaluaVtoOrd, e.Proveedor, p.Nombre").Rows
+            WDatos.ImportRow(row)
+        Next
+
+        WDatos.DefaultView.Sort = "FechaOrd ASC, Proveedor, Nombre"
+
+        dgvItems.DataSource = WDatos
+
+        For Each row As DataGridViewRow In dgvItems.Rows
+            If Val(row.Cells("Item").Value) = 0 Then
+                row.DefaultCellStyle.BackColor = Util.Clases.Globales.WBackColorTerciario
+            End If
+        Next
+    End Sub
+
+    Private Sub dgvItems_ColumnHeaderMouseClick(sender As Object, e As DataGridViewCellMouseEventArgs) Handles dgvItems.ColumnHeaderMouseClick
+
+        Dim tabla As DataTable = TryCast(dgvItems.DataSource, DataTable)
+
+        If tabla Is Nothing Then Exit Sub
+
+        Dim _dir As String
+
+        If dir = DireccionOrdenamiento.ASC Then
+            dir = DireccionOrdenamiento.DESC
+            _dir = "DESC"
+        Else
+            dir = DireccionOrdenamiento.ASC
+            _dir = "ASC"
+        End If
+
+        If e.ColumnIndex = dgvItems.Columns("Fecha").Index Then
+            tabla.DefaultView.Sort = "FechaOrd " + _dir
+        Else
+            tabla.DefaultView.Sort = dgvItems.Columns(e.ColumnIndex).DataPropertyName & " " & _dir
+        End If
+
+    End Sub
+
+    Private Enum DireccionOrdenamiento
+        ASC
+        DESC
+    End Enum
+
+    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        _CargarDatos()
+    End Sub
+
+    Private Sub txtDesde_KeyDown(sender As Object, e As KeyEventArgs) Handles txtDesde.KeyDown
+
+        If e.KeyData = Keys.Enter Then
+            If txtDesde.Text.Replace(" ", "").Length < 10 Then : Exit Sub : End If
+
+            txtHasta.Focus()
+
+        ElseIf e.KeyData = Keys.Escape Then
+            txtDesde.Text = ""
+        End If
+
+    End Sub
+
+    Private Sub txtHasta_KeyDown(sender As Object, e As KeyEventArgs) Handles txtHasta.KeyDown
+
+        If e.KeyData = Keys.Enter Then
+            If txtHasta.Text.Replace(" ", "").Length < 10 Then : Exit Sub : End If
+
+            _CargarDatos()
+
+        ElseIf e.KeyData = Keys.Escape Then
+            txtHasta.Text = ""
+        End If
+        
+    End Sub
+
+    Private Sub TextBox1_KeyUp(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyUp
+        Dim tabla As DataTable = TryCast(dgvItems.DataSource, DataTable)
+
+        If tabla IsNot Nothing Then tabla.DefaultView.RowFilter = String.Format("Proveedor LIKE '%{0}%' OR Nombre LIKE '%{0}%' OR DescItem LIKE '%{0}%' OR Fecha LIKE '%{0}%' OR Articulo LIKE '%{0}%' OR Descripcion LIKE '%{0}%'", TextBox1.Text)
+    End Sub
+
+    Private Sub VencimientosProximosEvaluaciones_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
+        TextBox1.Focus()
+    End Sub
+End Class
